74bd173b91c39b42ba258b568782a9a307d54f02
LwanJacs <lwanjacs@gmail.com>
2026-03-24 17:43:51 +0200

    creating db connection and restarting registration page outlook

--- registration.php
+++ registration.php
@@ -1,78 +1,58 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        
-        <!--tells browser what character encoding the page uses. 
-        Without it mignht not display some characters-->
-        <meta charset="utf-8">
-        
-        <!--width=device-width makes the page mathc device screen width.
-        initial scale=1.0 sets the initial zoom to normal (100%)-->
-        <meta name="viewport"
-        content="width=device-width, initial-scale=1.0">
-        
-        <!--Imports Font Awesome icons-->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        
-        <!--connets to externl css file for styling an customization-->
-        <link rel="stylesheet" href="new_style.css">
-        <title>Registraion</title>
-    </head>
+<?php
+include 'C:\xampp\htdocs\PHP\loginRegistrationSystem\database';
+$message = "";
+$toastClass = "";
+if($_SERVER["REQUEST_METHOD"] =="POST"){
+    $fname = $_POST['first_name'];
+    $lname = $_POST['last_name'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-    <body>
-        <!--creates a form where users can enter data. 
-        autocomplete="off" stops the browser from suggesting previously entered values.
-        class="form" connects this form to css styling-->
-        <form autocomplete="off" class="form">
-            <!--class connects to css file allowing it for it to be styled-->
-            
-            <p class="title">Registraion</p>
-            <p class="message">Signup now and get full access to our webpage.</p>
-            
-            <!--div is a container element.
-            class="form-group" is used for styling. 
-            This will make firstname and lastname sit next to each other.-->
-            <div class="form-group">
-                <label>
-                    <input type="text" required>
-                    
-                    <!---this for a float labeling effect to create a clean animation.-->
-                    <span>Firstname</span>
-                </label>
-                <label>
-                    <input type="text" required>
-                    <span>Lastname</span>
-                </label>
-            </div>
-            <label>
-                <!--wrong format must be added.-->
-                <input type="email" required>
-                <span>Email</span>
-            </label>
-            <label>
-                
-                <!--id used in javascript.-->
-                <input type="password" id="password" required>
-                <span>Password</span>
-                
-                <!-- Font awesome icon-->
-                <span class="icon" id="togglePassword">
-                    <i class="fa fa-eye-slash"></i>
-                </span>
-            </label>
-            <label>
-                <input type="password" id="passwordConfirm" required>
-                <span>Confirm password</span>
-                <span class="icon" id="togglePasswordConfirm">
-                    <i class="fa fa-eye-slash"></i>
-                </span>
-            </label>
-            <button class="submit">Submit</button>
-            <p class="signin">
-                Already have an account?
-                <a href="#">Signin</a>
-            </p>
-        </form>
-        <script src="main.js"></script>
-    </body>
+    //Check if email already exists
+    $checkEmailStmt = $conn->prepare("SELECT email FROM userdata WHERE email = ?");
+
+    $checkEmailStmt->bind_param("s", $email);
+    $checkEmailStmt->execute();
+    $checkEmailStmt->store_result();
+    //If number of rows returned is greater than 0, then email exists.
+    if($checkEmailStmt->num_rows > 0){
+        $message = "Email ID already exists";
+        $toastClass = "#007bff"; 
+    } else {
+        //Prepare and bind
+        $stmt = $conn->prepare("INSERT INTO userdata (first_name, last_name, username, email, password)
+        VALUES (?, ?, ?, ?, ?)");
+        //Safely inserts data into the table with a prepared statement. 'sssss' means all the values are string.
+        $stmt->bind_param("sssss", $fname, $lname, $username, $email, $hashedPassword);
+        
+        // Checking if account registered successfully
+        if($stmt->execute()) {
+            $message = "Account created successfully";
+            $toastClass = "#28a745";
+        } else {
+            $message = "Error: " .$stmt->error;
+            $toastClass = "#dc3545";
+        }
+
+        $stmt->close();
+    }
+    $checkEmailStmt->close();
+    $conn->close();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Registration</title>
+</head>
+<body>
+    
+</body>
 </html>
